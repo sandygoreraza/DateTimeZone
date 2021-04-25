a2dac892eb9595334aa5cd9f06dea1593632e637
sandygoreraza <gorerazasandy10@gmail.com>
2021-04-25 14:26:20 +0200

    Update TimezoneCalculator.php

--- TimezoneCalculator.php
+++ TimezoneCalculator.php
@@ -1,34 +1,37 @@
 <?php
+namespace Html;                                                                                                                                                                                                             use DateInterval;
+use DateTime;
+use DateTimeZone;
 
 class TimezoneCalculator{
 
-protected $TimeDate;
+    protected $TimeDate;
 
-public function __construct($TimeZoneSelected,$CATdatetime)
-{
+    public function __construct($TimeZoneSelected, $CATDateTime)
+    {
 
-  
-    $userTimezone=$this->userTimezone($TimeZoneSelected);
-    try {
-        $myDateTime = new DateTime($CATdatetime, $this->CAT_TIME_ZONE());
-    } catch (Exception $e) {
 
-   $myDateTime=null;
+        $userTimezone=$this->userTimezone($TimeZoneSelected);
+        try {
+            $myDateTime = new DateTime($CATDateTime, $this->CAT_TIME_ZONE());
+        } catch (Exception $e) {
+
+            $myDateTime=null;
+        }
+
+        $offset = $userTimezone->getOffset($myDateTime);
+        $myInterval=DateInterval::createFromDateString((string)$offset . 'seconds');
+        $myDateTime->add($myInterval);
+        $this->TimeDate=$myDateTime->format('Y-m-d H:i:s');
+
+        return  $this->TimeDate;
+
+
     }
 
-    $offset = $userTimezone->getOffset($myDateTime);
-    $myInterval=DateInterval::createFromDateString((string)$offset . 'seconds');
-    $myDateTime->add($myInterval);
-$this->TimeDate=$myDateTime->format('Y-m-d H:i:s');
-
-    return  $this->TimeDate;
-
-
-}
-
-private function CAT_TIME_ZONE(){
-    return new DateTimeZone('CAT');
-}
+    private function CAT_TIME_ZONE(){
+        return new DateTimeZone('CAT');
+    }
 
     private function userTimezone($timezone){
 
@@ -44,7 +47,4 @@ private function CAT_TIME_ZONE(){
 
 }
 
-//Get date and time for Los angeles time zone against CAT time
-$calculate_time=new TimezoneCalculator('America/Los_Angeles','2021-04-24 10:14');
 
-echo $calculate_time;
