8a5bbda788a34d0fd796dd073c54af3d86a1450c
sandygoreraza <gorerazasandy10@gmail.com>
2021-05-03 10:07:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include_once 'TimezoneCalculator.php';
 
 use Html\TimezoneCalculator;
 
-//Get date and time for Acre  time zone against CAT time
+//Get date and time for Brazil -Acre  time zone against CAT time
 $calculate_time=new TimezoneCalculator('Brazil/Acre', date("h:i:sa"));
 
 echo $calculate_time;
